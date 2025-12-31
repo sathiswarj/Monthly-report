87b68441dca13b2f95d8c6f81960b23231931b6c
Sathiswarj <slsjkr@gmail.com>
2025-12-31 15:51:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<DashboardLayout />}>
+        <Route path="/"  element={<DashboardLayout />}>
            <Route index element={<Dashboard />} />
         </Route>
       </Routes>
