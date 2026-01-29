71743b5282702a730be9d42cca10827743105866
Sathiswarj <slsjkr@gmail.com>
2026-01-29 18:47:51 +0530

    commit changes

--- logic/rest.js
+++
@@ -1,9 +0,0 @@
-// const [first, ...rest]  = [1,2,3,4,5]
-// console.log(first)
-// console.log(rest)
-
-// The JavaScript Rest parameter allows a function to accept an indefinite number of arguments as an array. It is represented by three dots (...) followed by the parameter name and must be the last parameter in the function, enabling flexible and dynamic argument handling.
-
-const {a,...rest} = {a:1,b:2,c:3}
-console.log(a)
-console.log(rest)
